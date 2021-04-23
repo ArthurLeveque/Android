afc6e46609f27cfc078538547aad7f0f77cd08fd
Arthur <arthur.lev44@gmail.com>
2021-04-23 21:15:57 +0200

    Activity

--- app/src/main/java/com/example/helloworld/ChildActivity.kt
+++ app/src/main/java/com/example/helloworld/ChildActivity.kt
@@ -1,13 +1,17 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class ChildActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
         setContentView(R.layout.activity_child)
+
+        val prenom: String? = intent.extras?.getString("prenom")
+        val nom: String? = intent.extras?.getString("nom")
+
+//        results.text = "Salut $prenom $nom !"
     }
 }
--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.example.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
+        setContentView(R.layout.activity_main)
 
         val myButton: Button = findViewById(R.id.button);
 
         myButton.setOnClickListener {
-            setContentView(R.layout.activity_child)
-        }
+            val i = Intent(this, ChildActivity::class.java)
+            var nom: EditText = findViewById(R.id.nom);
+            val prenom: EditText = findViewById(R.id.prenom);
 
-        setContentView(R.layout.activity_main)
+            i.putExtra ( "nom", nom.getText().toString() );
+            i.putExtra ( "prenom", prenom.getText().toString() );
+            startActivity(i)
+        }
     }
 
 
