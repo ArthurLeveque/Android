e932db9688959fc593280d064a295110b319ae0b
Charly Escalona <charly2221@hotmail.fr>
2021-04-30 14:05:29 +0200

    add handle error

--- app/src/main/java/com/example/helloworld/ChildActivity.kt
+++ app/src/main/java/com/example/helloworld/ChildActivity.kt
@@ -1,22 +1,22 @@
 package com.example.helloworld
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.helloworld.MainActivity.Companion.NOM_KEY
+import com.example.helloworld.MainActivity.Companion.PRENOM_KEY
 
 
 class ChildActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_child)
 
         val results: TextView = findViewById(R.id.results);
 
-        val prenom: String? = intent.extras?.getString("prenom")
-        val nom: String? = intent.extras?.getString("nom")
+        val prenom: String? = intent.extras?.getString(PRENOM_KEY)
+        val nom: String? = intent.extras?.getString(NOM_KEY)
 
         results.text = "Salut $prenom $nom !"
     }
--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -6,8 +6,16 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+        companion object{
+            const val NOM_KEY: String = "nom_key";
+            const val PRENOM_KEY: String = "prenom_key";
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,14 +24,19 @@ class MainActivity : AppCompatActivity() {
 
         myButton.setOnClickListener {
             val i = Intent(this, ChildActivity::class.java)
-            var nom: EditText = findViewById(R.id.nom);
+            val nom: EditText = findViewById(R.id.nom);
             val prenom: EditText = findViewById(R.id.prenom);
 
-            i.putExtra ( "nom", nom.getText().toString() );
-            i.putExtra ( "prenom", prenom.getText().toString() );
-            startActivity(i)
+            i.putExtra(NOM_KEY, nom.text.toString());
+            i.putExtra(PRENOM_KEY, prenom.text.toString());
+
+            if (nom.text.toString().isEmpty() || prenom.text.toString().isEmpty()) {
+                Toast.makeText(this, "Vous devez renseigner tous les champs !", Toast.LENGTH_SHORT)
+                    .show();
+            } else {
+                startActivity(i)
+            }
         }
     }
-
 
 }
