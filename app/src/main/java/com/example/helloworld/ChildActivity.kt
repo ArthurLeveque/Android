de7e42d7e89853e6637e2ca99e7244f6c73b813b
Arthur <arthur.lev44@gmail.com>
2021-04-30 13:07:24 +0200

    Affichage de données sur une autre activitée

--- app/src/main/java/com/example/helloworld/ChildActivity.kt
+++ app/src/main/java/com/example/helloworld/ChildActivity.kt
@@ -1,17 +1,23 @@
 package com.example.helloworld
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
 class ChildActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_child)
 
+        val results: TextView = findViewById(R.id.results);
+
         val prenom: String? = intent.extras?.getString("prenom")
         val nom: String? = intent.extras?.getString("nom")
 
-//        results.text = "Salut $prenom $nom !"
+        results.text = "Salut $prenom $nom !"
     }
 }
