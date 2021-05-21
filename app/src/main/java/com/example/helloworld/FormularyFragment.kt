98250cbfe21647438a0fb12d527d7247660f44cc
Arthur <arthur.lev44@gmail.com>
2021-05-21 14:02:32 +0200

    début fragment

--- app/src/main/java/com/example/helloworld/FormularyFragment.kt
+++ app/src/main/java/com/example/helloworld/FormularyFragment.kt
@@ -11,6 +11,23 @@ class FormularyFragment : Fragment(R.layout.fragment_formulary) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+        button.setOnClickListener {
+            val i = Intent(this, ChildActivity::class.java)
+
+            i.putExtra(NOM_KEY, nom.text.toString());
+            i.putExtra(PRENOM_KEY, prenom.text.toString());
+
+            if (nom.text!!.isEmpty()) {
+                nom.error = getString(R.string.error_nom)
+            } else if (prenom.text!!.isEmpty()) {
+                prenom.error = getString(R.string.error_prenom)
+            } else {
+                startActivity(i)
+            }
+        }
     }
 
 
